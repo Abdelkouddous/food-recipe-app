35ed640e7bbeec7f62cef8a3fa7b9a43679c9d92
Abdelkouddous <58855794+Abdelkouddous@users.noreply.github.com>
2024-08-26 13:04:15 +0100

    Final course version Project

--- foodapp/src/components/item.jsx
+++ foodapp/src/components/item.jsx
@@ -1,13 +1,19 @@
 export default function Item({ item }) {
   return (
-    <div style={{ flex: "row", display: "flex" }} className="">
-      <div>
-        <img
-          src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
-          className="h-12 w-12 rounded-full "
-        ></img>
-      </div>
-      <div>
+    <div
+      style={{
+        flex: "row",
+        display: "flex",
+        boxShadow: "1px 1px 2px 1px #ffffcc44",
+      }}
+      className="m-6 justify-around"
+    >
+      <img
+        src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
+        className="h-10 w-10 rounded-full p-1 m-2 "
+      ></img>
+
+      <div className="text-">
         <h3>{item.name}</h3>
         <h3>
           {item.amount} {item.unit}
